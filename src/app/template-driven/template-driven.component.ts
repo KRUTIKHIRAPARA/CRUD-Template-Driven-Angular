b3ff521c4688d4d241f587fa578a2fffeda10981
KRUTIKHIRAPARA <krutik2511@gmail.com>
2023-05-20 14:50:38 +0530

    Complete UI & Methods

--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Addressing, CrudTempletDrivenService, Datas, PersonalData } from '../services/crud-templet-driven.service';
-import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-template-driven',
@@ -11,56 +11,141 @@ import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class TemplateDrivenComponent {
 
+  // Table Column Name
   displayedColumns: string[] = ['id', 'companyName', 'fName', 'lName', 'email', 'mobileNo', 'address', 'salary', 'adharNo', 'panNo', 'passbookNo', 'actions'];
+
+  // Dynamic Angular Material Table Reference
   dataSource = new MatTableDataSource<any>;
 
-  closeResult = '';
-
+  // Dynamic Angular Material Table Pagination
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
+  // Get & Store All Datas Array
   allDatas?: Array<Datas> = new Array<Datas>();
 
+  // Data Binding
   getDatas: Datas;
+
+  // Update & Add Button Toggle
+  btnUDToogle: boolean = true; 
+
+  // Search Value
+  searchVal:string;
 
   constructor(private _crud: CrudTempletDrivenService, private modalService: NgbModal) { }
 
   ngOnInit(): void {
 
+    // Data Binding Create New Reference
     this.getDatas = new Datas;
     this.getDatas.address = new Addressing;
     this.getDatas.pernalDetails = new PersonalData;
 
+    // Get Data Method
     this.getDetail();
   }
 
-  ngAfterViewInit() {
+  // Pagenations Methods
+  pagenations(){
     this.dataSource.paginator = this.paginator;
   }
 
+  // Add Module Open Method ( - Angular Material -)
   open(content) {
-    this.modalService.open(content, { size: 'lg' })
+    this.modalService.open(content, { size: 'lg' });
   }
 
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
-  }
-
+  // Get All Data In API Method
   getDetail() {
     this._crud.getData().subscribe({
       next: (res) => {
         this.allDatas = res;
         this.dataSource = new MatTableDataSource(this.allDatas);
+        this.dataSource.paginator = this.paginator;
       },
       error: (err) => {
         console.log(err);
       }
     });
+  }
+
+  // Add Data In API Method
+  addDetail() {
+    this._crud.addData(this.getDatas).subscribe({
+      next: (res) => {
+        this.getDatas = new Datas;
+        this.getDatas.address = new Addressing;
+        this.getDatas.pernalDetails = new PersonalData;
+        this.getDetail();
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
+  // Fill Data In Edit Model
+  fillDetail(data){
+    this.getDatas = data;
+    this.btnUDToogle = false;
+  }
+
+  // Dismiss Details
+  dismissDetail(){
+    this.getDatas = new Datas;
+    this.getDatas.address = new Addressing;
+    this.getDatas.pernalDetails = new PersonalData;
+    this.btnUDToogle = true;
+  }
+
+  // Update Data In API Method
+  updateDetail(){
+    this._crud.editData(this.getDatas).subscribe({
+      next: (res) => {
+        this.getDatas = new Datas;
+        this.getDatas.address = new Addressing;
+        this.getDatas.pernalDetails = new PersonalData;
+        this.btnUDToogle = true;
+        this.getDetail();
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
+  // Delete Data In API Method
+  deleteDetail(body){
+    this._crud.deleteData(body).subscribe({
+      next: (res) => {
+        this.getDatas = new Datas;
+        this.getDatas.address = new Addressing;
+        this.getDatas.pernalDetails = new PersonalData;
+        this.getDetail();
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
+  // Search Datas Method
+  searchDeatil() {
+    if (this.searchVal) {
+      let searchEmployee = new Array<Datas>();
+      if (this.allDatas.length > 0) {
+        for (let emp of this.allDatas) {
+          if (JSON.stringify(emp).toLowerCase().indexOf(this.searchVal.toLowerCase()) > 0) {
+            searchEmployee.push(emp);
+          }
+        }
+        this.dataSource = new MatTableDataSource(searchEmployee);
+        // this.allDatas = searchEmployee;
+      }
+    }
+    else {
+      this.getDetail();
+    }
   }
 
 }
